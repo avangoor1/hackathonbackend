54b977a1be76b5848554b6a61770c6debbb65da7
avangoor1 <ananya.vangoor@gmail.com>
2022-11-12 10:48:24 -0600

    POST endpoint added.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
+app.post("/upload-data",(request,response) => {
+    //code to perform particular action.
+    //To access POST variable use req.body()methods.
+    console.log(request.body);
+    response.send(request.body);
+    });
 
 app.get("/", (req, res) => {
     res.send("Hello Ananya's World");
